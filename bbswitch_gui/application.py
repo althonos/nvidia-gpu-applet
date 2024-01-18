c9b809b138015ab779d60897230aaa7c4d30aeb0
Martin Larralde <martin.larralde@embl.de>
2024-01-18 21:07:53 +0100

    Remove code handling the state switching

--- bbswitch_gui/application.py
+++ bbswitch_gui/application.py
@@ -29,8 +29,8 @@ MODULE_LOAD_TIMEOUT = 5  # How long to wait until nvidia module become accessibl
 class Application(Gtk.Application):
     """Main application class allowing only one running instance."""
 
-    bbswitch = BBswitchMonitor()
-    client = BBswitchClient()
+    # bbswitch = BBswitchMonitor()
+    # client = BBswitchClient()
     nvidia = NvidiaMonitor(timeout=REFRESH_TIMEOUT)
 
     def __init__(self, *args, **kwargs) -> None:
@@ -73,7 +73,7 @@ class Application(Gtk.Application):
 
         bus_id, enabled, device, vendor = '', False, '', ''
         try:
-            bus_id, enabled = self.bbswitch.get_gpu_state()
+            bus_id, enabled = self.nvidia.default_bus_id(), True  #self.bbswitch.get_gpu_state()
             vendor, device = PCIUtil.get_device_info(PCIUtil.get_vendor_id(bus_id),
                                                      PCIUtil.get_device_id(bus_id))
         except BBswitchMonitorException as err:
@@ -166,24 +166,9 @@ class Application(Gtk.Application):
         """
         if not self.window:
             self.window = MainWindow(self)
-            self.window.connect('power-state-switch-requested', self._on_state_switch)
             self.window.connect('delete-event', self._on_window_close)
             self.window.connect('show', self._on_window_show)
             self.window.connect('hide', self._on_window_hide)
-
-            # Ping server so it will load bbswitch module
-            try:
-                self.client.send_command('status')
-            except BBswitchClientException as err:
-                message = str(err)
-                logging.error(message)
-                self.window.error_dialog(
-                    'BBswitch client error',
-                    message + '\n\n'
-                    + 'If this happened right after installation, check the following:\n'
-                    + ' • Service "bbswitchd" is enabled and running\n'
-                    + ' • Active user is in effective group "bbswitchd"\n'
-                    + ' • You have rebooted or restarted loginctl session')
         else:
             self.window.set_keep_above(True)
             self.window.deiconify()
@@ -194,7 +179,6 @@ class Application(Gtk.Application):
             self.indicator = Indicator()
             self.indicator.connect('open-requested', self._on_activate)
             self.indicator.connect('exit-requested', self._on_quit)
-            self.indicator.connect('power-state-switch-requested', self._on_state_switch)
 
     def do_command_line(self, *args: Gio.ApplicationCommandLine, **kwargs) -> int:
         """Handle command line arguments.
@@ -215,26 +199,27 @@ class Application(Gtk.Application):
         initialized = self.window is not None
 
         self.activate()
+        self._enabled_gpu = self.nvidia.default_bus_id()
 
         if self.window:
             # Utility function for starting bbswitch monitor
-            def bbswitch_monitor_start(window=None, is_active=None):
-                if window:
-                    window.disconnect(connection)
-                    if not is_active or not window.get_property(is_active.name):
-                        return
-                self.bbswitch.monitor_start(self.update_bbswitch)
+            # def bbswitch_monitor_start(window=None, is_active=None):
+            #     if window:
+            #         window.disconnect(connection)
+            #         if not is_active or not window.get_property(is_active.name):
+            #             return
+            #     self.bbswitch.monitor_start(self.update_bbswitch)
 
             if 'minimize' in options:
-                if not initialized:
+                # if not initialized:
                     # Start bbswitch monitor right now
-                    bbswitch_monitor_start()
+                    # bbswitch_monitor_start()
                 self._bg_notification_shown = True
                 self.window.hide()
             else:
-                if not initialized:
+                # if not initialized:
                     # Start bbswitch monitor after window was activated
-                    connection = self.window.connect('notify::is-active', bbswitch_monitor_start)
+                    # connection = self.window.connect('notify::is-active', bbswitch_monitor_start)
                 self.window.show()
 
         return 0
@@ -251,38 +236,6 @@ class Application(Gtk.Application):
         self.withdraw_notification('running_in_bg')
         self.quit()
         return GLib.SOURCE_REMOVE
-
-    def _on_state_switch_finish(self, error):
-        if error is not None:
-            logger.error(str(error))
-            self.update_bbswitch()
-            self.nvidia.monitor_stop()
-            if self.window and self._enabled_gpu:
-                self.update_nvidia(self._enabled_gpu, 0)
-            self._notify_error('Failed to switch power state', str(error))
-        if self.window:
-            self.window.set_cursor_arrow()
-
-    def _on_state_switch(self, widget, state):
-        del widget  # unused argument
-        if self.client.in_progress():
-            self.client.cancel()
-            return
-
-        self._switch_time = time.monotonic()
-        if not state and self._enabled_gpu:
-            # Update GPU info
-            self.update_nvidia(self._enabled_gpu, 0)
-            if self.gpu_info and len(self.gpu_info['processes']) > 0:
-                self._notify_error('NVIDIA GPU is in use',
-                                   'Please stop processes using it first')
-                return
-            self.nvidia.monitor_stop()
-
-        # Switch to opposite state
-        self.client.set_gpu_state(state, self._on_state_switch_finish)
-        if self.window:
-            self.window.set_cursor_busy()
 
     def _notify_error(self, title, message):
         if self.window and self.window.is_active():
